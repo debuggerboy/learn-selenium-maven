0ff3e53e66004824d53f457d9dd3b77000728c0e
debuggerboy <debuggerboy@gmail.com>
2023-02-11 20:09:18 +0530

    pushing latest changes

--- src/test/java/com/pulsiot/testing/AppTest.java
+++ src/test/java/com/pulsiot/testing/AppTest.java
@@ -2,19 +2,61 @@ package com.pulsiot.testing;
 
 import static org.junit.Assert.assertTrue;
 
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 import org.junit.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 /**
- * Unit test for simple App.
+ * Hello world!
+ *
  */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    public static void main() {
+        // Set Driver path
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        WebDriver driver=new ChromeDriver();
+
+        //open google
+        driver.get("https://www.google.com");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        //enter linux tutorials in search box
+        driver.findElement(By.name("q")).sendKeys("Linux Tutorials");
+
+        //wait for suggestions
+        //WebDriverWait wait=new WebDriverWait(driver, 20);
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("sbtc")));
+
+        WebElement list=driver.findElement(By.className("sbtc"));
+        //List rows=list.findElements(By.tagName("li"));
+        List<WebElement> rows = list.findElements(By.tagName("li"));
+
+        System.out.println("Rows count is: "+rows .size());
+
+        for(WebElement elem:rows) {
+            System.out.println(elem.getText());
+        }
+
+    }
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
 }
+
